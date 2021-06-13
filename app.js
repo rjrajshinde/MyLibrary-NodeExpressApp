8fa59cb6211cd6a9b7dd0910b675aedc0e736aa4
rjrajshinde <rjrajshinde@gmail.com>
2021-06-13 18:45:55 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,57 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var expressLayouts = require('express-ejs-layouts')
+require('dotenv').config()
+var mongoose = require('mongoose');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Database Connection
+// mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true  }).then((result)=>{
+//   console.log('Connected to DB')
+// }).catch((err)=>{
+//   console.log(err)
+// })
+mongoose.connect(process.env.DB_URL,{useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+    if (err)
+        throw err;
+    else
+        console.log("Database Successfully Connected");
+});
+
+//routes
+var indexRouter = require('./routes/index');
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
